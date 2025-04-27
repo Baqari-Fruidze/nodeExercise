6c8a73edbe34afb77178d909f066c7f2e23847c7
Baqari-Fruidze <154324770+Baqari-Fruidze@users.noreply.github.com>
2025-04-28 00:07:25 +0400

    midlware

--- app.js
+++ app.js
@@ -2,18 +2,15 @@ import express from "express";
 import fs from "fs";
 const app = express();
 app.use(express.json());
+app.use((req, res, next) => {
+  console.log("ola midllware");
+  next();
+});
 
 // Helper function to read fresh data
 const readProductsData = () => {
   return JSON.parse(fs.readFileSync("./data/products.json", "utf8"));
 };
-
-// const date = ()=>{
-//   return   {const date = new Date()
-//   const year = date.getFullYear()
-//   const month = date.getMonth() + 1
-//   const day = date.getDate();}
-// }
 
 // function calculateDate() {
 //   const date = new Date();
@@ -24,31 +21,11 @@ const readProductsData = () => {
 //   return createdAt;
 // }
 
-app.get("/products", (req, res) => {
-  const products = JSON.parse(fs.readFileSync("./data/products.json", "utf8"));
+const getProducts = (req, res) => {
+  const products = readProductsData();
   res.json(products);
-  console.log(products);
-});
-// პროდუქტების რაოდენის დათვლა
-app.get("/products/count", (req, res) => {
-  const products = readProductsData();
-  const count = products.length;
-  res.json({ count });
-});
-
-// ყველაზე ძვირიანი
-app.get("/products/most-expensive", (req, res) => {
-  const products = readProductsData();
-  let mostExpensiveProduct = products[0];
-  products.forEach((product) => {
-    product.price > mostExpensiveProduct.price
-      ? (mostExpensiveProduct = product)
-      : null;
-  });
-  res.status(201).json(mostExpensiveProduct);
-});
-// ახალი პროდუქტის დამატება
-app.post("/products", (req, res) => {
+};
+const createProducts = (req, res) => {
   const products = readProductsData();
 
   const newProduct = {
@@ -71,19 +48,9 @@ app.post("/products", (req, res) => {
   } else {
     res.status(409).send("product already exists");
   }
-});
+};
 
-// ყველას წაშლა
-app.delete("/products/delete-all", (req, res) => {
-  fs.writeFileSync("./data/products.json", "[]");
-  const products = readProductsData();
-  res
-    .status(200)
-    .json({ message: "All products have been deleted successfully" });
-});
-
-// პროდუქტის შეცვლა
-app.put("/products/:id", (req, res) => {
+const editProduct = (req, res) => {
   const products = readProductsData();
   const productIndex = products.findIndex(
     (product) => product.id === Number(req.params.id)
@@ -93,20 +60,16 @@ app.put("/products/:id", (req, res) => {
   products[productIndex] = newProduct;
   fs.writeFileSync("./data/products.json", JSON.stringify(products));
   res.json(newProduct);
-});
-
-// წაშლა
-app.delete("/products/:id", (req, res) => {
+};
+const deleteProduct = (req, res) => {
   const products = readProductsData();
   const newData = products.filter(
     (product) => product.id !== Number(req.params.id)
   );
   fs.writeFileSync("./data/products.json", JSON.stringify(newData));
   res.status(204).send("product deleted");
-});
-
-// ყიდვა და რაოდენობაზე ერთს გამოკლება
-app.post("/buy/:id", (req, res) => {
+};
+const buyProduct = (req, res) => {
   const products = readProductsData();
   const productIndex = products.findIndex(
     (product) => product.id === Number(req.params.id)
@@ -124,7 +87,51 @@ app.post("/buy/:id", (req, res) => {
   products[productIndex] = targetProduct;
   fs.writeFileSync("./data/products.json", JSON.stringify(products));
   res.json(targetProduct);
-});
+};
+
+/////////////////////////////////////////////////////////////////////////////
+
+// // პროდუქტების მიღება
+// app.get("/products", getProducts);
+// // ახალი პროდუქტის დამატება
+// app.post("/products", createProducts);
+app.route("/products").get(getProducts).post(createProducts); // საერთო როუტი ერთანირი სახელების მქონეებისთვის
+// პროდუქტის შეცვლა
+// app.put("/products/:id", editProduct);
+// // წაშლა
+// app.delete("/products/:id", deleteProduct);
+app.route("products/:id").put(editProduct).delete(deleteProduct); //saerTo routi
+
+// ყიდვა და რაოდენობაზე ერთს გამოკლება
+app.post("/products/buy/:id", buyProduct);
+app.route("/products/buy/:id").post(buyProduct);
+// პროდუქტების რაოდენის დათვლა
+// app.get("/products/count", (req, res) => {
+//   const products = readProductsData();
+//   const count = products.length;
+//   res.json({ count });
+// });
+
+// ყველაზე ძვირიანი
+// app.get("/products/most-expensive", (req, res) => {
+//   const products = readProductsData();
+//   let mostExpensiveProduct = products[0];
+//   products.forEach((product) => {
+//     product.price > mostExpensiveProduct.price
+//       ? (mostExpensiveProduct = product)
+//       : null;
+//   });
+//   res.status(201).json(mostExpensiveProduct);
+// });
+
+// ყველას წაშლა
+// app.delete("/products/delete-all", (req, res) => {
+//   fs.writeFileSync("./data/products.json", "[]");
+//   const products = readProductsData();
+//   res
+//     .status(200)
+//     .json({ message: "All products have been deleted successfully" });
+// });
 
 app.listen(3000, () => {
   console.log("your server is running on port 3000");
