f60377b714d480a73dc7e0464edb7d6d4e95a71b
Baqari-Fruidze <154324770+Baqari-Fruidze@users.noreply.github.com>
2025-05-06 00:08:02 +0400

    db

--- server.js
+++ server.js
@@ -1,6 +1,17 @@
 import app from "./app.js";
+import mongoose from "mongoose";
 
 const port = process.env.PORT || 3000;
+const DB_URL = process.env.DB_URL.replace(
+  "<PASSWORD>",
+  process.env.DB_PASS
+).replace("<DB_USERNAME>", process.env.DB_USER);
+mongoose
+  .connect(DB_URL)
+  .then(() => {
+    console.log("Database conected");
+  })
+  .catch((err) => console.log(err));
 app.listen(port, () => {
   console.log("your server is running on port 3000");
 });
