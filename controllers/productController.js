d486acbf64f1ffefc981cad98881353de466e8e2
Baqari-Fruidze <154324770+Baqari-Fruidze@users.noreply.github.com>
2025-05-07 00:52:42 +0400

    put

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,8 +1,11 @@
 import fs from "fs";
 import mongoose from "mongoose";
 
-const readProductsData = () => {
-  return JSON.parse(fs.readFileSync("./data/products.json", "utf8"));
+const readProductsData = async () => {
+  // return JSON.parse(fs.readFileSync("./data/products.json", "utf8"));
+  const Product = mongoose.model("Product", productSchema);
+  const products = await Product.find({});
+  return products;
 };
 
 const productSchema = new mongoose.Schema({
@@ -14,15 +17,21 @@ const productSchema = new mongoose.Schema({
   slug: { type: String },
   createdAt: { type: Date, default: Date.now },
 });
+productSchema.set("toJSON", {
+  transform: (doc, ret) => {
+    delete ret.__v;
+    return ret;
+  },
+});
 const Product = mongoose.model("Product", productSchema);
 
 const getProducts = async (req, res) => {
-  const products = await Product.find({});
+  const products = await readProductsData();
   console.log(products);
   res.json(products);
 };
-const createProducts = (req, res) => {
-  const products = readProductsData();
+const createProducts = async (req, res) => {
+  const products = await readProductsData();
 
   const newProduct = {
     ...req.body,
@@ -37,26 +46,53 @@ const createProducts = (req, res) => {
   if (!newProduct.price || !newProduct.name) {
     return res.status(400).send("name and price are required fields");
   } else if (uniqueChecker) {
-    products.push(newProduct);
-    fs.copyFileSync("./data/products.json", "./data/products_backup.json");
-    fs.writeFileSync("./data/products.json", JSON.stringify(products));
+    // products.push(newProduct);
+    // fs.copyFileSync("./data/products.json", "./data/products_backup.json");
+    // fs.writeFileSync("./data/products.json", JSON.stringify(products));
+    await Product.create(newProduct);
+
     res.status(201).json(newProduct);
   } else {
     res.status(409).send("product already exists");
   }
 };
 
-const editProduct = (req, res) => {
-  const products = readProductsData();
-  const productIndex = products.findIndex(
-    (product) => product.id === Number(req.params.id)
-  );
-  // products[productIndex] = { ...products[productIndex], newProduct };
-  const newProduct = { ...products[productIndex], ...req.body };
-  products[productIndex] = newProduct;
-  fs.writeFileSync("./data/products.json", JSON.stringify(products));
-  res.json(newProduct);
+// const editProduct = async(req, res) => {
+//   const products =  await readProductsData();
+//   const productIndex = products.findIndex(
+//     (product) => product.id === Number(req.params.id)
+//   );
+//   // products[productIndex] = { ...products[productIndex], newProduct };
+//   const newProduct = { ...products[productIndex], ...req.body };
+//   products[productIndex] = newProduct;
+//   fs.writeFileSync("./data/products.json", JSON.stringify(products));
+//   res.json(newProduct);
+// };
+
+const editProduct = async (req, res) => {
+  const { id } = req.params;
+  const updatedProduct = req.body;
+
+  try {
+    // Find and update the product by its id
+    const product = await Product.findOneAndUpdate(
+      { id: Number(id) }, // Find product by the 'id' field
+      updatedProduct, // The updated data
+      { new: true } // Return the updated document (default is to return the old one)
+    );
+
+    if (!product) {
+      return res.status(404).send("Product not found");
+    }
+
+    // Send the updated product back as the response
+    res.status(200).json(product);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("An error occurred while updating the product");
+  }
 };
+
 const deleteProduct = (req, res) => {
   const products = readProductsData();
   const newData = products.filter(
